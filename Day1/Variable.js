8c09547ef1d0a0e45ca1daedb9baa5623ee66fd4
ayush844 <ayush.xyz1625@gmail.com>
2023-12-25 12:48:53 +0530

    variables in JS

---
+++ Day1/Variable.js
@@ -0,0 +1,26 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur" //this way is possible to assign memory but it is not recommended
+
+
+let accountState; //when we only declare variable and don't define it its value becomes undefined
+
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+when we declare variable with var it don't lie inside a particular scopre and becomes global variable
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
