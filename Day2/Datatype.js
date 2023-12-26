00b257104870b8381282e6f04637a881561e425f
ayush844 <ayush.xyz1625@gmail.com>
2023-12-26 23:43:45 +0530

    revised datatype in JS

---
+++ Day2/Datatype.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser will not run in terminal
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+
+// string => ""
+
+// boolean => true/false
+
+// null => standalone value [empty]
+
+// undefined => variable declared but not defined
+
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+
+console.log(typeof null); // object
